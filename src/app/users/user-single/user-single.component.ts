769594994740ae653a64efa6b9a5454747a397f9
meron93 <meronyemane18@gmail.com>
2018-12-11 20:13:44 -0800

    Single user router implemented

--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-user-single',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-single.component.scss']
 })
 export class UserSingleComponent implements OnInit {
-
-  constructor() { }
+  user;
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+    ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const userId = params['userid'];
+      this.userService
+        .getUser(userId)
+        .subscribe(user => this.user = user);
+    });
   }
-
 }
