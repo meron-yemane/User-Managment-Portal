7d8d6724164cdda926b41d4b614bc41684e93283
meron93 <meronyemane18@gmail.com>
2018-12-14 14:52:05 -0800

    user-edit component created, shadow added to navbar

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserSingleComponent } from './user-single/user-single.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
-  declarations: [UserListComponent, UserSingleComponent],
+  declarations: [UserListComponent, UserSingleComponent, UserEditComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
