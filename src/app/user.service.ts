b04b43603bd2a75ad22e94470aa080e0fcde0a91
meron93 <meronyemane18@gmail.com>
2018-12-16 20:36:42 -0800

    createUser function created

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
   apiUrl = 'http://localhost:3000/users';
   constructor(private http: HttpClient) {}
 
@@ -13,5 +21,9 @@ export class UserService {
   }
 
   editUser() {
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}`, user, this.httpOptions);
   }
 }
--- src/app/users/user-create/user-create.component.ts
+++ src/app/users/user-create/user-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'models/user';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.scss']
 })
 export class UserCreateComponent implements OnInit {
-
-  constructor() { }
+  user: User = {
+    name: null,
+    email: null,
+    login: null,
+    roles: []
+  };
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  onCheckBoxChange(event, role) {
+    if (this.user.roles.indexOf(role) > -1) {
+      this.user.roles.splice(this.user.roles.indexOf(role), 1);
+    } else {
+      this.user.roles.push(role);
+    }
+  }
+
+  processUserCreateForm() {
+    this.userService.createUser(this.user).subscribe(
+      (data: User) => {
+        console.log(data);
+      }
+    );
   }
 
 }
