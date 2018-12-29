e3983789491ad8beb837207c425ba9974e035efd
meron93 <meronyemane18@gmail.com>
2018-12-28 17:59:42 -0800

    create-role component created

--- src/app/roles/roles-routing.module.ts
+++ src/app/roles/roles-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RolesListComponent } from './roles-list/roles-list.component';
 import { RolesSingleComponent } from './roles-single/roles-single.component';
+import { CreateRoleComponent } from './create-role/create-role.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: ':role',
     component: RolesSingleComponent
+  },
+  {
+    path: 'create',
+    component: CreateRoleComponent
   }
 ];
 
--- src/app/roles/roles.module.ts
+++ src/app/roles/roles.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RolesRoutingModule } from './roles-routing.module';
 import { RolesListComponent } from './roles-list/roles-list.component';
 import { RolesSingleComponent } from './roles-single/roles-single.component';
+import { CreateRoleComponent } from './create-role/create-role.component';
 
 @NgModule({
-  declarations: [RolesListComponent, RolesSingleComponent],
+  declarations: [RolesListComponent, RolesSingleComponent, CreateRoleComponent],
   imports: [
     CommonModule,
     RolesRoutingModule
