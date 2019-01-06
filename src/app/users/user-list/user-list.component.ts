bdf96c256b62fe54bcc6fa194552163415494405
meron93 <meronyemane18@gmail.com>
2019-01-05 18:10:29 -0800

    moved user filtering logic from pipe into component for improved control and efficiency

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user.service';
+import { User } from 'models/user';
 
 @Component({
   selector: 'app-user-list',
@@ -10,7 +11,21 @@ export class UserListComponent implements OnInit {
   users;
   result;
   allUsers;
-  searchTerm: string;
+  private _searchTerm: string;
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredUsers = this.filterUsers(value);
+  }
+  filteredUsers: User[];
+
+  filterUsers(searchString: string) {
+    return this.users.filter(user => {
+      return user.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
+    });
+  }
 
   constructor(private userService: UserService) { }
 
@@ -27,7 +42,10 @@ export class UserListComponent implements OnInit {
 
   ngOnInit() {
     this.userService.getUsers().subscribe(
-      allUsers => this.users = this.activeUsers(allUsers)
+      allUsers => {
+        this.users = this.activeUsers(allUsers);
+        this.filteredUsers = this.users;
+      }
     );
   }
 }
